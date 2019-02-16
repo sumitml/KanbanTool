629386156ab4384402c99ef88dec1af0f218981d
sumit.ml@gmail.com <sumit.ml@gmail.com>
2019-02-17 00:27:00 +0530

    first commit

---
+++ src/main/java/com/sk/ppm/KanbanManagementToolApplication.java
@@ -0,0 +1,14 @@
+package com.sk.ppm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanManagementToolApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sk/ppm/KanbanManagementToolApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sk.ppm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KanbanManagementToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
